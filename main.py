bbeea30a37801dfa79157107fca4032ff89fc9e1
Henry Ning <henryhuning@gmail.com>
2021-03-04 10:14:53 +0000

    99. [Interactive Coding Exercise] Days in Month 7min

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-#Functions with Outputs
-def format_name(f_name, l_name):
-  if f_name == "" or l_name == "":
-    return "You didn't provide valid inputs."
-  formated_f_name = f_name.title()
-  formated_l_name = l_name.title()
-  f"Result: {formated_f_name} {formated_l_name}"
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
 
-#Storing output in a variable
-formatted_name = format_name(input("Your first name: "), input("Your last name: "))
-print(formatted_name)
-#or printing output directly
-print(format_name(input("What is your first name? "), input("What is your last name? ")))
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+  if is_leap(year) and month == 2:
+    return 29
+  return month_days[month-1]
+  
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
 
-#Already used functions with outputs.
-length = len(formatted_name)
 
-#Return as an early exit
-def format_name(f_name, l_name):
-  """Take a first and last name and format it 
-  to return the title case version of the name."""
-  if f_name == "" or l_name == "":
-    return "You didn't provide valid inputs."
-  formated_f_name = f_name.title()
-  formated_l_name = l_name.title()
-  return f"Result: {formated_f_name} {formated_l_name}"
+
+
+
+
+
+
+
+
+
+
