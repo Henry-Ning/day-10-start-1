26d1adb7541b54214e654c65a742d4008b4664d6
Henry Ning <henryhuning@gmail.com>
2021-03-06 19:04:04 +0000

    Quiz 8: Functions Quiz

--- main.py
+++ main.py
@@ -29,6 +29,11 @@ month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(days)
 
+# What you knowThese are questions you got right on the first try.
+# Without running the code below, what do you think will be printed in the console? def add(n1, n2): return n1 + n2 def subtract(n1, n2): return n1 - n2 def multiply(n1, n2): return n1 * n2 def divide(n1, n2): return n1 / n2 print(add(2, multiply(5, divide(8, 4))))
+# What would you predict to be the result of running the following code?def outer_function(a, b): def inner_function(c, d): return c + d return inner_function(a, b) result = outer_function(5, 10) print(result)
+# What will be printed in the console after running the following code?def my_function(a): if a < 40: return print
+
 
 
 
